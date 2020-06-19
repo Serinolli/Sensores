25333fded9f01f606eaf8481f6662460af2f3999
Serinolli <rafael.serinolli2@gmail.com>
2020-06-18 23:42:39 -0300

    Leitor de QRCode

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Dialogs } from '@ionic-native/dialogs/ngx';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +10,59 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+public corpoPagina: HTMLElement;
+public img: HTMLElement;
+
+public scanner : any;
+
+  constructor(private qrScanner: QRScanner, private dialogs: Dialogs, public platform: Platform) {
+    this.platform.backButton.subscribeWithPriority(0, ()=>{
+
+      this.corpoPagina.style.opacity = "1";
+      this.img.style.opacity = "1";
+
+      this.qrScanner.hide();
+      this.scanner.unsubscribe(); //stop scanner
+
+    });
+  }
+
+  public lerQrCode(){
+    this.qrScanner.prepare()
+  .then((status: QRScannerStatus) => {
+     if (status.authorized) {
+       // camera permission was granted
+          this.qrScanner.show();
+
+          this.corpoPagina = document.getElementsByTagName('ion-content')[0] as HTMLElement;
+          this.corpoPagina.style.opacity = "0";
+
+          this.img = document.getElementById('logo') as HTMLElement;
+          this.img.style.opacity = "0"
+
+       // start scanning
+       this.scanner = this.qrScanner.scan().subscribe((text: string) => {
+         console.log('Scanned something', text);
+
+         this.dialogs.alert('Resultado da leitura ' + text);
+
+         this.corpoPagina.style.opacity = "1";
+         this.img.style.opacity = "1";
+
+         this.qrScanner.hide(); // hide camera preview
+         this.scanner.unsubscribe(); // stop scanning
+       });
+
+     } else if (status.denied) {
+       // camera permission was permanently denied
+       // you must use QRScanner.openSettings() method to guide the user to the settings page
+       // then they can grant the permission from there
+     } else {
+       // permission was denied, but not permanently. You can ask for permission again at a later time.
+     }
+  })
+  .catch((e: any) => console.log('Error is', e));
+
+  }
 
 }
